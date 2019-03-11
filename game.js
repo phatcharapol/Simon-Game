0d81a4350c24cdf0d053921a59ac1bd7328b60be
NUT-ITE <nuttt.x3@gmail.com>
2019-03-11 12:04:09 +0700

    Complete Simon Game V1.0.0

--- game.js
+++ game.js
@@ -1,63 +1,63 @@
 var buttonColor = ["red", "blue", "yellow", "green"];
+
 var gamePattern = [];
-var randomChoosenColor = "";
-var userClickedPattern = [];
-var userChoosenColor = "";
+
 var level = 0;
-var i = 0 ;
+var userCntClick = 0;
 
 $(document).keypress(function (e) {
+    // Press SpaceBar And Level 1 will be start ;
     if (e.keyCode === 32 && level === 0) {
         nextSequence();
     }
 });
 
+// User Choose Color
 $('.btn').click(function () {
-    userChoosenColor = this.id;
-    userClickedPattern.push(userChoosenColor);
+    var userChoosenColor = this.id;
     animatePressed(userChoosenColor);
-
-    checkAnswer(userChoosenColor,i,level);
-   
-})
+    playSound(userChoosenColor);
+    checkAnswer(userChoosenColor, userCntClick, level);
+});
 
 
 function nextSequence() {
+    // Random Color index [0-3]
     var randNumber = (Math.floor(Math.random() * 4));
-    randomChoosenColor = buttonColor[randNumber];
+    var randomChoosenColor = buttonColor[randNumber];
+
     $("." + randomChoosenColor).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChoosenColor);
 
     gamePattern.push(buttonColor[randNumber]);
     level++;
     $("#level-title").text("Level " + level);
-    i=0;
-  
+    userCntClick = 0;
 }
 
-function checkAnswer(userClicked,numClick,level) {
-   
-   
-        if (userClicked == gamePattern[numClick]) {
-            console.log("success");
-            i=numClick++ ;
-            if(numClick == level){
+// Check Color When User Clicked
+function checkAnswer(userClicked, numClick, level) {
+
+    if (userClicked == gamePattern[numClick]) {
+        userCntClick++;
+        if (numClick == level - 1) {
+            setTimeout(function () {
                 nextSequence();
-            }
-        } else {
-            console.log("wrong");
-            gameOver();
+            }, 700);
         }
-        console.log(userClicked + "---" + gamePattern[numClick]+"**"+gamePattern);
-        console.log(numClick + "---" + level);
-       
+    } else {
+        gameOver();
+    }
+
 }
 
+// Play Sound Animation
 function playSound(color) {
     var audio = new Audio('sounds/' + color + '.mp3');
     audio.play();
 }
 
+// Animation when Pressed
 function animatePressed(color) {
     $(".btn." + color).addClass("pressed")
     setTimeout(function () {
@@ -65,13 +65,15 @@ function animatePressed(color) {
     }, 100);
 }
 
+// Show Game Over And Reset All Varible ;
 function gameOver() {
     $("body").addClass("game-over");
     setTimeout(function () {
         $("body").removeClass("game-over")
-    }, 100);
+    }, 200);
     playSound("wrong");
     $("#level-title").text("Game Over, Press Any Key to Restart");
     level = 0;
-    i=0;
+    userCntClick = 0;
+    gamePattern = [];
 }
